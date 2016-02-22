be9697c64ea5f31e08ebfa2ade505fbfb147a090
Dmitry Shintyakov <shintyakov@gmail.com>
2016-02-22 20:32:28 +0300

    fix: google analytics injector

--- ext/inject_google_analytics.py
+++ ext/inject_google_analytics.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 TRACK_ID = 'UA-74087176-1'
 
 code="""
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  (function(i,s,o,g,r,a,m){{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){{
+  (i[r].q=i[r].q||[]).push(arguments)}},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  }})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', {TRACK_ID}, 'auto');
   ga('send', 'pageview');
