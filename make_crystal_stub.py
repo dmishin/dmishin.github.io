a46d6d28530c4410c075ea7f18584881429d7103
Dmitry Shintyakov <shintyakov@gmail.com>
2017-06-16 01:23:43 +0300

    fix: update stub maker for new ark

--- make_crystal_stub.py
+++ make_crystal_stub.py
@@ -16,27 +16,34 @@ def make_stub(folder):
     try:
         os.makedirs(os.path.join("src",photos_source))
     except: pass
+    try:
+        os.makedirs(os.path.join("res",photos_source))
+    except: pass
     
     for name in os.listdir(photos_orig):
         print(name)
         if os.path.splitext(name)[1].lower() not in (".jpg", ".jpeg", ".png"):
             continue
 
-        opath = os.path.join("src",photos_source, name.lower())
+        opath = os.path.join("res",photos_source, name.lower())
         ipath = os.path.join(photos_orig, name)
         shutil.copy(ipath, opath)
         print("copy", ipath, opath )
         image_names.append(os.path.join("@root",photos_source, name.lower()))
             
     #writing post
-    post_file = os.path.join("src","[crystals]", compound_name+".md")
+    post_file = os.path.join("src","crystals", compound_name+".md")
 
     title = input("Enter title: ").strip()
     formula = input("Enter forumla: ").strip()
     tags = input("Enter tags: ").strip()
+    
+    imagelist = "\n".join(f"![{title}]({iname})\n\n"
+                          for iname in image_names)
+    
 
     with open(post_file, "w") as post:
-        post.write("""---
+        post.write(f"""---
 title: {title}
 tags: {tags}
 ---
@@ -51,11 +58,10 @@ Formula: {{{formula}}}
 ## Growing
 ## Safety
 ## More photos
+{imagelist}
 ## References
-""".format(**locals()))
-                   
-        for iname in image_names:
-            post.write("![{title}]({iname})\n\n".format(**locals()))
+""")
+
                    
     
 if __name__=="__main__":
